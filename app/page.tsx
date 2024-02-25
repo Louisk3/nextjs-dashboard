25845de51c41e7f7d34d9fcacbfc1ffddec45be9
Louisk3 <156825888+Louisk3@users.noreply.github.com>
2024-02-25 02:39:43 -0800

    Replace img tag to Image component

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <img
+          {/* <img
             src={HeroDesktopImg.src}
             alt="HeroImage"
             width={1000}
@@ -44,8 +44,14 @@ export default function Home() {
             width={560}
             height={620}
             className="block md:hidden"
+          /> */}
+          <Image
+            src="/hero-desktop.png"
+            alt="HeroImage"
+            width={1000}
+            height={760}
+            className="hidden md:block"
           />
-          {/*HeroImage here*/}
         </div>
       </div>
     </main>

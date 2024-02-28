756df8cd771d16dbbc0347dbf6ac65c0a6ea9cd8
Louisk3 <156825888+Louisk3@users.noreply.github.com>
2024-02-28 04:58:33 -0800

    Chapter 10 partial prerendering

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,6 +2,14 @@ import { lusitana } from "@/app/ui/fonts";
 import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { fetchCardData } from "@/app/lib/data";
+import { Suspense } from "react";
+import CardWrapper from "@/app/ui/dashboard/cards";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+  CardSkeleton,
+} from "@/app/ui/dashboard/skeletons";
 
 export default async function page() {
   return (
@@ -10,18 +18,17 @@ export default async function page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart />
-        <LatestInvoices />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
